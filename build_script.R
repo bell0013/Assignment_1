4833b2585aa3f7e6b0333cb4cf6388c3137f6c4e
bell0013 <bell0013@student.monash.edu>
2023-11-14 21:23:41 +1100

    add library commandss

--- build_script.R
+++ build_script.R
@@ -1,1 +1,19 @@
 #Script area for building markdown functions
+
+
+ai_data <- read.csv("ai_numbers.csv", check.names = FALSE)
+
+head(ai_data)
+
+
+install.packages("tidyverse")
+
+install.packages("Rtools")
+
+chooseCRANmirror(ind=1) # Choose a different mirror index
+
+chooseCRANmirror(ind=1, useHTTPS=TRUE, mirrors = "https://cran.rstudio.com/")
+
+chooseCRANmirror(ind=1)
+
+
